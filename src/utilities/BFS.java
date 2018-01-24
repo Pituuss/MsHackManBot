2276cfdd783c8457abc4b67b82e4a2426967df1a
Piotr Wojtyś <pituuuss@gmail.com>
2018-01-24 19:23:31 +0100

    Add refactors

---
+++ src/utilities/BFS.java
@@ -0,0 +1,111 @@
+package utilities;
+
+
+import bot.BotState;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+@SuppressWarnings("ALL")
+public class BFS {
+    private Boolean[][] visited;
+    private Point[][] parent;
+    private Double[][] distance;
+    private LinkedList<Point> queue;
+    private int width;
+    private int height;
+
+    public BFS(int width, int height) {
+        this.width = width;
+        this.height = height;
+        visited = new Boolean[width][height];
+        parent = new Point[width][height];
+        distance = new Double[width][height];
+        queue = new LinkedList<>();
+    }
+
+    public void clear() {
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                distance[i][j] = Double.POSITIVE_INFINITY;
+                parent[i][j] = null;
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    public ArrayList<Point> getNeighbours(Point position, BotState state) {
+        ArrayList<Point> adjacentPoints = new ArrayList<>();
+
+        int posX = position.x;
+        int posY = position.y;
+
+        Point upper = new Point(posX, posY - 1);
+        Point bottom = new Point(posX, posY + 1);
+        Point left = new Point(posX - 1, posY);
+        Point right = new Point(posX + 1, posY);
+
+        if (state.getField().getField()[posX][posY].contains("Gl"))
+            adjacentPoints.add(new Point(width - 1, posY));
+        if (state.getField().getField()[posX][posY].contains("Gr"))
+            adjacentPoints.add(new Point(0, posY));
+        if (state.getField().isPointValid(upper)) adjacentPoints.add(upper);
+        if (state.getField().isPointValid(bottom)) adjacentPoints.add(bottom);
+        if (state.getField().isPointValid(left)) adjacentPoints.add(left);
+        if (state.getField().isPointValid(right)) adjacentPoints.add(right);
+
+        return adjacentPoints;
+    }
+
+    public void startBFS(Point pos, BotState state) {
+
+        distance[pos.x][pos.y] = 0.0;
+        visited[pos.x][pos.y] = true;
+
+        markBugs(visited, state);
+
+        queue.push(pos);
+        while (!queue.isEmpty()) {
+            Point point = queue.pop();
+            for (Point ngh : getNeighbours(point, state)) {
+                if (!visited[ngh.x][ngh.y]) {
+                    queue.add(ngh);
+                    visited[ngh.x][ngh.y] = true;
+                    parent[ngh.x][ngh.y] = point;
+                    distance[ngh.x][ngh.y] = distance[point.x][point.y] + 1;
+                }
+            }
+        }
+    }
+
+    public void markBugs(Boolean[][] visited, BotState state) {
+        for (Point bugPos : state.getField().getEnemyPositions()) {
+            visited[bugPos.x][bugPos.y] = true;
+            if (bugPos.x - 1 >= 0)
+                visited[bugPos.x - 1][bugPos.y] = true;
+            if (bugPos.x + 1 < this.width)
+                visited[bugPos.x + 1][bugPos.y] = true;
+            if (bugPos.y - 1 >= 0)
+                visited[bugPos.x][bugPos.y - 1] = true;
+            if (bugPos.y + 1 < this.height)
+                visited[bugPos.x][bugPos.y + 1] = true;
+        }
+    }
+
+    public Point[][] getParent() {
+        return parent;
+    }
+
+    public Double[][] getDistance() {
+        return distance;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ src/utilities/BugManager.java
@@ -0,0 +1,101 @@
+package utilities;
+
+import bot.BotState;
+import move.MoveType;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+@SuppressWarnings("ALL")
+public class BugManager {
+    private int width, height;
+    private Boolean[][] visited;
+    private Double[][] distance;
+
+    public BugManager(int width, int height) {
+        this.height = height;
+        this.width = width;
+        this.visited = new Boolean[width][height];
+        this.distance = new Double[width][height];
+    }
+
+    public int nearbyBugsCount(BotState state) {
+        int counter = 0;
+        LinkedList<Point> queue = new LinkedList<>();
+
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                distance[i][j] = Double.POSITIVE_INFINITY;
+                visited[i][j] = false;
+            }
+        }
+        Point pos = state.getField().getMyPosition();
+        distance[pos.x][pos.y] = 0.0;
+        visited[pos.x][pos.y] = true;
+
+        queue.push(pos);
+        while (!queue.isEmpty()) {
+            Point point = queue.pop();
+            for (Point ngh : bugGetNeighbours(point, state)) {
+                if (!visited[ngh.x][ngh.y]) {
+                    queue.add(ngh);
+                    visited[ngh.x][ngh.y] = true;
+                    distance[ngh.x][ngh.y] = distance[point.x][point.y] + 1;
+                }
+            }
+        }
+
+        for (Point bug : state.getField().getEnemyPositions()) {
+            if (distance[bug.x][bug.y] < 3.0)
+                counter += 1;
+        }
+        return counter;
+    }
+
+    private ArrayList<Point> bugGetNeighbours(Point position, BotState state) {
+        ArrayList<Point> adjacentPoints = new ArrayList<>();
+
+        int posX = position.x;
+        int posY = position.y;
+
+        Point upper = new Point(posX, posY - 1);
+        Point bottom = new Point(posX, posY + 1);
+        Point left = new Point(posX - 1, posY);
+        Point right = new Point(posX + 1, posY);
+
+        if (state.getField().isPointValid(upper)) adjacentPoints.add(upper);
+        if (state.getField().isPointValid(bottom)) adjacentPoints.add(bottom);
+        if (state.getField().isPointValid(left)) adjacentPoints.add(left);
+        if (state.getField().isPointValid(right)) adjacentPoints.add(right);
+
+        return adjacentPoints;
+    }
+
+    public boolean bugAlert(BotState state) {
+        return nearbyBugsCount(state) > 0;
+    }
+
+    public MoveType bugByYou(MoveType move, BotState state) {
+        Point pos = state.getField().getMyPosition();
+
+        int posX = pos.x;
+        int posY = pos.y;
+
+        Point upper = new Point(posX, posY - 1);
+        Point bottom = new Point(posX, posY + 1);
+        Point left = new Point(posX - 1, posY);
+        Point right = new Point(posX + 1, posY);
+
+        if (move.equals(MoveType.PASS) && state.getField().getEnemyPositions().contains(upper))
+            return MoveType.DOWN;
+        if (move.equals(MoveType.PASS) && state.getField().getEnemyPositions().contains(bottom))
+            return MoveType.UP;
+        if (move.equals(MoveType.PASS) && state.getField().getEnemyPositions().contains(left))
+            return MoveType.RIGHT;
+        if (move.equals(MoveType.PASS) && state.getField().getEnemyPositions().contains(right))
+            return MoveType.LEFT;
+
+        return move;
+    }
+}
